c1acc2c234f87b0e0f42b4af763b4db8092e29f8
raojiajun <471412450@qq.com>
2018-03-21 10:26:36 +0800

    滚动优化

--- js/lookWhatAndShow.js
+++ js/lookWhatAndShow.js
@@ -17,7 +17,9 @@
         addHeightLightAndremoveOffset()
         //添加heightlight效果
         function addHeightLightAndremoveOffset(){
-            var closeIndex = findCloseIndex(scrollHeight, divTops)
+            var closeIndex = findCloseIndex(scrollHeight + 200, divTops)
+            console.log(scrollHeight)
+            console.log(closeIndex)
             for (var i = 0; i < divTops.length; i++){
                 heightTags[i].classList.remove("active")
             }
